1cb147faec8b08308b9c4b387761b7af5c644a5f
Diego Toro <dtoro.a.s@gmail.com>
2021-08-01 18:16:36 -0400

    Opcion por default de un panel de control para el reproductor

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,15 @@
 import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
 
 const movie = document.querySelector('.movie')
-const playButton = document.querySelector('#playButton')
-const muteButton = document.querySelector('#muteButton')
+const pannelControl = document.querySelector('.pannel-control')
 
-const player = new MediaPlayer({ el: movie })
+const player = new MediaPlayer({ 
+  media: movie, 
+  pannelControl: pannelControl, 
+  plugins: [
+    new AutoPlay()
+  ] 
+})
 
-playButton.onclick = () => player.togglePlay(playButton)
-muteButton.onclick = () => player.toggleMute(muteButton)
+player.displayPannelControl()
